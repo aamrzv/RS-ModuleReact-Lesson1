c65a28bb4a94f3793b4809fd5e5f0ac88504a385
aamrz <aamrz@LAPTOP-O1PV5R9F>
2024-01-19 19:15:54 +0300

    Задание 1 пункты 9

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,20 @@
-// Декларативный стиль
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// Декларативный стиль
 function App() {
-  // Декларативный стиль
-  return (
-    // Декларативный стиль
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          // Декларативный стиль
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          {new Date().getFullYear()}
-        </p>
-      </header>
-    </div>
+  const appDiv = React.createElement('div', { className: 'App' },
+    React.createElement('header', { className: 'App-header' },
+      React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+      React.createElement('p', null,
+        React.createElement('code', null, 'Edit ', React.createElement('code', null, 'src/App.js'), ' and save to reload.')
+      ),
+      React.createElement('a', { className: 'App-link', href: 'https://reactjs.org', target: '_blank', rel: 'noopener noreferrer' }, 'Learn React'),
+      React.createElement('p', null, new Date().getFullYear())
+    )
   );
+
+  return appDiv;
 }
 
-// Декларативный стиль
 export default App;
